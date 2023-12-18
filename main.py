82abb14e5e8e80f033a8a1f447059107f4b52b5d
brypro <bperoliva@gmail.com>
2023-12-17 21:13:41 -0300

    imagenes

--- main.py
+++ main.py
@@ -1,24 +1,40 @@
+import random
 import pygame
 import math
 
 pygame.init()
 
 # tamaño de la ventana
-WHDTH, HEIGHT = 800, 600
+WHDTH, HEIGHT = 1280, 720
 ventana = pygame.display.set_mode((WHDTH, HEIGHT))
 
 pygame.display.set_caption("GRAVITAS")
 
-MASA_PLANETA = 100
+MASA_PLANETA = 200
 MASA_NAVE = 5
-GRAVEDAD = 1
+GRAVEDAD = 20
 FPS = 60
-PLANETA_SIZE = 50 #radio
-NAVE_SIZE = 5
+PLANETA_SIZE = 100 #radio
+NAVE_SIZE = 20
 VELOCIDAD = 100
 
+
+#devuelve un numero random entre los valores minimos y maximos
+def random_num(min, max):
+    return random.randint(min, max)
+
 BG = pygame.transform.scale(pygame.image.load("fondo.jpg"), (WHDTH, HEIGHT))
-#PLANETA = pygame.transform.scale(pygame.image.load("planeta.png"), (PLANETA_SIZE * 2, PLANETA_SIZE * 2))
+PLANETA = pygame.transform.scale(pygame.image.load(f"{random_num(1,5)}.png"), (PLANETA_SIZE * 2, PLANETA_SIZE * 2))
+
+nves = [
+    pygame.transform.scale(pygame.image.load("7.png"), (NAVE_SIZE * 2, NAVE_SIZE * 2)),
+    pygame.transform.scale(pygame.image.load("8.png"), (NAVE_SIZE * 2, NAVE_SIZE * 2)),
+    pygame.transform.scale(pygame.image.load("9.png"), (NAVE_SIZE * 2, NAVE_SIZE * 2)),
+    pygame.transform.scale(pygame.image.load("10.png"), (NAVE_SIZE * 2, NAVE_SIZE * 2)),
+    pygame.transform.scale(pygame.image.load("11.png"), (NAVE_SIZE * 2, NAVE_SIZE * 2)),
+    pygame.transform.scale(pygame.image.load("12.png"), (NAVE_SIZE * 2, NAVE_SIZE * 2)),
+    pygame.transform.scale(pygame.image.load("13.png"), (NAVE_SIZE * 2, NAVE_SIZE * 2))
+]
 
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
@@ -31,8 +47,7 @@ class Planeta:
         self.masa = masa
 
     def draw(self):
-        #ventana.blit(PLANETA, (self.x - PLANETA_SIZE, self.y - PLANETA_SIZE))
-        pygame.draw.circle(ventana, GREEN, (self.x, self.y), PLANETA_SIZE )
+        ventana.blit(PLANETA, (self.x - PLANETA_SIZE, self.y - PLANETA_SIZE))
 
 class Nave:
     def __init__(self, x:int, y:int, vel_x, vel_y, masa):
@@ -41,17 +56,28 @@ class Nave:
         self.vel_x = vel_x
         self.vel_y = vel_y
         self.masa = masa
+        self.num = self.random_nave(7,13)
+    
+    def random_nave(self,min, max):
+        return random.randint(min, max)
+    
     
     def draw(self):
-        pygame.draw.circle(ventana, RED, (self.x, self.y), NAVE_SIZE)
+        ventana.blit(nves[self.num-7], (self.x , self.y ))
     
     def move(self, planeta = None):
+        #distancia entre la nave y el planeta con formula de pitagoras (d = raiz cuadrada de (x2 - x1)**2 + (y2 - y1)**2)
         distancia = math.sqrt((self.x - planeta.x)**2 + (self.y - planeta.y)**2)
+        #fuerza de atraccion entre la nave y el planeta con la formula de la gravedad universal (F = G * (m1 * m2) / d**2
         fuerza = (GRAVEDAD * (self.masa * planeta.masa) ) / distancia**2
+        #aceleracion de la nave con la formula de la segunda ley de newton (a = F / m)
         aceleracion = fuerza / self.masa
+        #angulo de la nave con la formula trigonometrica de la tangente inversa (atan2(y2 - y1, x2 - x1))
         angulo = math.atan2(planeta.y - self.y, planeta.x - self.x)
-        
+        #aceleracion en x con la formula trigonometrica de la aceleracion (a = cos(angulo) * aceleracion)
+        #la aceleracion en x es la componente x de la aceleracion
         aceleracion_x = math.cos(angulo) * aceleracion
+        #aceleracion en y con la formula trigonometrica de la aceleracion (a = sin(angulo) * aceleracion)
         aceleracion_y = math.sin(angulo) * aceleracion
         
         self.vel_x += aceleracion_x
@@ -68,6 +94,9 @@ def create_nave(localizacion, mouse):
     velocidad_y = (mouse_y - temp_y) / VELOCIDAD
     nave= Nave(temp_x, temp_y, velocidad_x, velocidad_y, MASA_NAVE)
     return nave
+
+
+
 
 def main():
     run = True
@@ -99,14 +128,14 @@ def main():
         for nv in naves[:]:
             nv.draw()
             nv.move(planeta=planeta)
-            fuera_pantalla = nv.x < 0 or nv.x > WHDTH or nv.y < 0 or nv.y > HEIGHT
-            choque = math.sqrt((nv.x - planeta.x)**2 + (nv.y - planeta.y)**2) < PLANETA_SIZE + NAVE_SIZE
+            fuera_pantalla = nv.x < 0 or nv.x > (WHDTH *1.2) or nv.y < 0 or nv.y > (HEIGHT * 1.2)	
+            choque = math.sqrt((nv.x - planeta.x)**2 + (nv.y - planeta.y)**2) < PLANETA_SIZE + NAVE_SIZE - 20
             if fuera_pantalla or choque:
                 naves.remove(nv)
         
         if temp_nave_pos:
             pygame.draw.line(ventana, WHITE, temp_nave_pos, mouse_pos, 2)
-            pygame.draw.circle(ventana, RED, temp_nave_pos, NAVE_SIZE)
+            pygame.draw.circle(ventana, GREEN, temp_nave_pos, NAVE_SIZE)
         
         planeta.draw()
         pygame.display.update()
